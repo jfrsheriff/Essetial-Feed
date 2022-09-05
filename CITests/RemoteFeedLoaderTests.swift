bfe0dabf12a7da0b8a1530e9e3ad09a117fe479c
Jaffer Sheriff <jfrsheriff@gmail.com>
2022-09-05 22:39:01 +0530

    Http Client protocol Implementation

--- CITests/RemoteFeedLoaderTests.swift
+++ CITests/RemoteFeedLoaderTests.swift
@@ -9,20 +9,45 @@ import XCTest
 
 
 class RemoteFeedLoader {
+    let client : HTTPClient
+    
+    init(client : HTTPClient){
+        self.client = client
+    }
+    
+    func load(){
+        client.get(from: URL(string: "https://www.google.com")!)
+    }
 }
 
-class HTTPClient{
+protocol HTTPClient{
+    func get(from url : URL)
+}
+
+class HTTPClientSpy : HTTPClient{
     var requestedUrl : URL?
+    
+    func get(from url : URL){
+        self.requestedUrl = url
+    }
 }
 
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromUrl(){
-        let client = HTTPClient()
-        _ = RemoteFeedLoader()
-        XCTAssertNil(client.requestedUrl)
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
         
+        XCTAssertNil(client.requestedUrl)
+    }
+    
+    func test_init_requestDataFromUrl(){
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        
+        sut.load()
+        XCTAssertNotNil(client.requestedUrl)
     }
     
 }
