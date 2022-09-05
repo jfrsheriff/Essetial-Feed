fea47f0858499ace30994f29a4c4be001c2acea3
Jaffer Sheriff <jfrsheriff@gmail.com>
2022-09-05 19:59:33 +0530

    Initial Commit

---
+++ Essetial Feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Essetial Feed
+//
+//  Created by Jaffer Sheriff U on 05/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
