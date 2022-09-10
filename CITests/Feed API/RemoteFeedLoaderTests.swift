bc1db084b8f3c99aa907124107ca1e33a1371047
Jaffer Sheriff <jfrsheriff@gmail.com>
2022-09-10 12:25:43 +0530

    Tests Improvements

--- CITests/Feed API/RemoteFeedLoaderTests.swift
+++ CITests/Feed API/RemoteFeedLoaderTests.swift
@@ -94,6 +94,20 @@ class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_doesNotDeliverResultAfterSUTHasBeenDeallocated(){
+        let url = URL(string: "https:a-url.com")!
+        let client = HTTPClientSpy()
+        var sut : RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load(){capturedResults.append($0)}
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJson(items: []))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(url : URL = URL(string: "www.a-given-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut : RemoteFeedLoader, client : HTTPClientSpy ) {
         let client = HTTPClientSpy()
