43b5009fcda8433f3f782e93e00b26bbd76979d0
Jaffer Sheriff <jfrsheriff@gmail.com>
2022-09-14 23:03:22 +0530

    Memory Leak test added

--- CITests/Feed API/URLSessionHTTPClientTests.swift
+++ CITests/Feed API/URLSessionHTTPClientTests.swift
@@ -78,8 +78,16 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     
-    private func makeSUT() -> URLSessionHTTPClient{
-        URLSessionHTTPClient()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient{
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func trackForMemoryLeak ( _ instance : AnyObject , file: StaticString = #filePath, line: UInt = #line){
+        addTeardownBlock {[weak instance] in
+            XCTAssertNil(instance, "Instance Should Have Been Deallocation . Potential Memory Leak", file: file, line: line)
+        }
     }
     
     private class URLProtocolStub : URLProtocol {
