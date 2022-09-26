17cc6aaaddc862dbda2b5042bb48cafdafea0831
Jaffer Sheriff <jfrsheriff@gmail.com>
2022-09-26 22:58:09 +0530

    Travis CI Added

---
+++ CIAPIEndToEndTests/CIAPIEndToEndTests.swift
@@ -0,0 +1,19 @@
+//
+//  CIAPIEndToEndTests.swift
+//  CIAPIEndToEndTests
+//
+//  Created by Jaffer Sheriff U on 26/09/22.
+//
+
+import XCTest
+import CI
+
+class CIAPIEndToEndTests: XCTestCase {
+    
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData(){
+        
+    
+ 
+    }
+
+}
